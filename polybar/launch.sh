879964027061eaf7bffeb0250edac3768e232ca7
cqroot <cqroot@outlook.com>
2022-10-22 00:33:31 +0800

    feat(polybar): remove github module

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -3,18 +3,20 @@
 script_dir=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 args=(-c "${script_dir}/config.ini" --reload)
 
-killall -q polybar
+pgrep polybar | xargs kill -9
+
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 if [ "$(xrandr | grep -c "connected")" -gt 1 ]; then
     for m in $(polybar --list-monitors | cut -d":" -f1); do
         if [ "$m" == "HDMI-1" ]; then
-            MAINMON=$m POLYBAR_DIR=${script_dir} polybar "${args[@]}" main &
+            MAINMON=$m POLYBAR_DIR=${script_dir} polybar "${args[@]}" main 2>&1 &
         else
-            NOTRAYMON=$m POLYBAR_DIR=${script_dir} polybar "${args[@]}" notray &
+            NOTRAYMON=$m POLYBAR_DIR=${script_dir} polybar "${args[@]}" notray 2>&1 &
         fi
     done
 else
-    POLYBAR_DIR=${script_dir} polybar "${args[@]}" main 2>&1 | tee -a /tmp/polybar.log &
+    POLYBAR_DIR=${script_dir} polybar "${args[@]}" main 2>&1 | tee -a /tmp/polybar.log 2>&1 &
 fi
+
 disown
